a862a47992dbb79ba6c2043e430a7dbaadf75e86
DaoPhialuang <92082938+DaoPhialuang@users.noreply.github.com>
2022-07-15 15:41:59 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+
+    var audios = new Audio("audio/paj.wav");
+    audios.play();
+
+    var twm = new Audio("audio/twm.wav");
+    twm.play();
+    
+    var xauv = new Audio("audio/xauv.wav");
+    twm.play();
+
+    var kos = new Audio("audio/kos.wav");
+    twm.play();
+
+    var suab = new Audio("audio/suab.wav");
+    twm.play();
+       
+       
+       
+
+
+
+
